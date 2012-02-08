f0605c388fc277800b0e701709104ec2e757052d
Dimitrios Amaxilatis <amaxilat@cti.gr>
2012-02-08 17:29:19 +0200

    initial import

---
+++ 0.I.11/src/main/java/eu/uberdust/MainApp.java
@@ -0,0 +1,19 @@
+package eu.uberdust;
+
+
+import eu.uberdust.lights.LightController;
+
+/**
+ * Main App.
+ */
+public class MainApp {
+
+    /**
+     * Main Function.
+     *
+     * @param args String Arguments
+     */
+    public static void main(final String[] args) {
+        LightController.getInstance();
+    }
+}
---
+++ 0.I.11WS/src/main/java/eu/uberdust/MainApp.java
@@ -0,0 +1,13 @@
+package eu.uberdust;
+
+import eu.uberdust.lights.LightController;
+
+
+/**
+ * The Main Class of the App.
+ */
+public class MainApp {
+    public static void main(final String[] args) {
+        LightController.getInstance();
+    }
+}
