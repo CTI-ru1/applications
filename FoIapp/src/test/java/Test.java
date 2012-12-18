be6d58573a090ef38ae3ab31823e693ec1a40657
DiMmiOus <jim_dimakopoulos@msn.com>
2012-12-18 16:20:10 +0200

    delay fix

--- FoIapp/src/test/java/Test.java
+++ FoIapp/src/test/java/Test.java
@@ -1,7 +1,12 @@
+import eu.uberdust.MainApp;
 import eu.uberdust.communication.UberdustClient;
+import eu.uberdust.lights.GetJson;
 import org.apache.log4j.Logger;
 
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
 import java.io.IOException;
+import java.util.Properties;
 
 /**
  * Created by IntelliJ IDEA.
@@ -16,11 +21,37 @@ public class Test {
 
     public static void main(final String[] args) {
 
+
+        Properties prop = new Properties();
+
         try {
-            UberdustClient.getInstance().sendCoapPost("2df", "lz1", "1");
-        } catch (IOException e) {
-            LOGGER.error(e, e);
+            //load a properties file
+            prop.load(new FileInputStream("config.properties"));
+
+            //get the property value and print it out
+            System.out.println(prop.getProperty("FOI"));
+            System.out.println(prop.getProperty("ZONES"));
+
+        } catch (IOException ex) {
+            ex.printStackTrace();
         }
+
+        //try {
+            //set the properties value
+         //   prop.setProperty("FOI", "workstation:0.I.2-2");
+         //   prop.setProperty("ZONES", "1");
+
+            //save properties to project root folder
+         //   prop.store(new FileOutputStream("config.properties"), null);
+
+        //} catch (IOException ex) {
+        //    ex.printStackTrace();
+        //}
+
+       // System.out.println(GetJson.getInstance().callGetJsonWebService("http://150.140.16.31/api/v1/foi?identifier=workstation:0.I.2-2","mode"));  //350
+       //UberdustClient.getInstance().sendCoapPost("2df", "lz1", "1");
+
  }
+
 
 }
