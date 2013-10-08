584ae80a5227d7cc9e760ff98b3a67a366ae6572
Dimitrios Amaxilatis <amaxilat@cti.gr>
2013-10-08 12:17:04 +0300

    .

--- FoIapp/src/main/java/eu/uberdust/application/foi/MainApp.java
+++ FoIapp/src/main/java/eu/uberdust/application/foi/MainApp.java
@@ -17,9 +17,9 @@ public class MainApp {
     public static String MODE;
     public static String ACTUATOR_MODE;
     public static String[] ZONES;
-    public static final String CAPABILITY_LIGHT = "urn:wisebed:node:capability:light";
-    public static final String CAPABILITY_PIR = "urn:wisebed:node:capability:pir";
-    public static final String CAPABILITY_SCREENLOCK = "urn:wisebed:ctitestbed:node:capability:lockScreen";
+    public static final String CAPABILITY_LIGHT = "urn:node:capability:light";
+    public static final String CAPABILITY_PIR = "urn:node:capability:pir";
+    public static final String CAPABILITY_SCREENLOCK = "urn:node:capability:lockScreen";
 
 
     //public static final String SENSOR_LIGHT_READINGS_REST = "http://uberdust.cti.gr/rest/testbed/1/node/urn:wisebed:ctitestbed:virtual:room:0.I.2/capability/urn:wisebed:node:capability:light/tabdelimited/limit/" + FoiController.WINDOW;
@@ -37,7 +37,7 @@ public class MainApp {
         ProfileManager.getInstance().setIdentifier(FOI);
         MODE = ProfileManager.getInstance().getElement("mode");
 
-        if( MODE.equals("ichatzWorkstation") || MODE.equals("Workstation") ){
+        if("ichatzWorkstation".equals(MODE) || "Workstation".equals(MODE)){
 
             ACTUATOR_MODE = "workstation";
 
--- listenerWS/src/main/java/eu/uberdust/applications/listenerws/ListenerApp.java
+++ listenerWS/src/main/java/eu/uberdust/applications/listenerws/ListenerApp.java
@@ -1,6 +1,8 @@
 package eu.uberdust.applications.listenerws;
 
+import eu.uberdust.communication.UberdustClient;
 import eu.uberdust.communication.protobuf.Message;
+import eu.uberdust.communication.rest.UberdustRestClient;
 import eu.uberdust.communication.websocket.readings.WSReadingsClient;
 import org.apache.log4j.Logger;
 import org.apache.log4j.PropertyConfigurator;
@@ -24,6 +26,7 @@ public class ListenerApp implements Observer {
      * static instance(ourInstance) initialized as null.
      */
     private static ListenerApp ourInstance = null;
+    private long lastEvent;
     private String server;
     private String protocol;
 
@@ -39,24 +42,27 @@ public class ListenerApp implements Observer {
     public ListenerApp() {
         PropertyConfigurator.configure(this.getClass().getClassLoader().getResource("log4j.properties"));
         LOGGER.info("hello world!");
+//        server = "ws://uberdust.cti.gr:80/readings.ws";
         server = "ws://uberdust.cti.gr:80/readings.ws";
         // Call http://carrot.cti.gr:8080/uberdust/rest/testbed/1/node/urn:wisebed:ctitestbed:0x9979/capability/urn:wisebed:node:capability:pir/insert/timestamp/1731231000/reading/1/
         // to add a reading
 
         WSReadingsClient.getInstance().setServerUrl(server);
-        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:virtual:room:0.I.11", "urn:wisebed:node:capability:pir");
+//        WSReadingsClient.getInstance().subscribe("urn:testing:0x3ef", "urn:node:capability:get1");
 //        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:odyssey", "urn:wisebed:ctitestbed:node:capability:lockScreen");
 //        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:virtual:room:0.I.2", "urn:wisebed:node:capability:light");
 //        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:virtual:0x295", "urn:wisebed:node:capability:light");
-//        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:0x180", "urn:wisebed:node:capability:light");
+        WSReadingsClient.getInstance().subscribe("urn:amaxilat:virtual:room:1", "urn:node:capability:lz1");
+        WSReadingsClient.getInstance().subscribe("urn:amaxilat:virtual:room:1", "urn:node:capability:pir");
 //        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:0x180", "urn:wisebed:node:capability:temperature");
 //        WSReadingsClient.getInstance().subscribe("urn:wisebed:ctitestbed:0x153d", "urn:wisebed:node:capability:temperature");
 
         LOGGER.info("Starting connection with Server:" + server);
         LOGGER.info("Starting connection with protocol:" + protocol);
 
-
+        UberdustClient.setUberdustURL("http://uberdust.cti.gr:80");
         WSReadingsClient.getInstance().addObserver(this);
+        lastEvent = 0;
     }
 
     @Override
@@ -68,6 +74,16 @@ public class ListenerApp implements Observer {
         if (arg instanceof Message.NodeReadings) {
             Message.NodeReadings reading = (Message.NodeReadings) arg;
             LOGGER.info(reading.getReading(0).getNode());
+            if (reading.getReading(0).getCapability().contains("pir")) {
+                if (reading.getReading(0).getDoubleReading() > 0) {
+                    UberdustClient.getInstance().sendCoapPost("dfa", "lz1", "1");
+                    lastEvent = System.currentTimeMillis();
+                } else {
+                    if (System.currentTimeMillis() - lastEvent > 20000) {
+                        UberdustClient.getInstance().sendCoapPost("dfa", "lz1", "0");
+                    }
+                }
+            }
         }
     }
 }
